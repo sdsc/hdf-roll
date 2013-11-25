dafff75ca0b9ca30386b3d01e4860cbfc20f7ee3
Trevor Cooper <tcooper@sdsc.edu>
2013-11-25 11:28:46 -0800

    Updated Makefile and version.mk to use pull.mk

--- src/hdf4/version.mk
+++ src/hdf4/version.mk
@@ -1,3 +1,20 @@
-NAME    = hdf4_$(ROLLCOMPILER)
-VERSION = 2.9
-RELEASE = 0
+NAME               = hdf4_$(ROLLCOMPILER)
+VERSION            = 2.9
+RELEASE            = 0
+PKGROOT            = /opt/hdf4/$(ROLLCOMPILER)
+
+SRC_SUBDIR         = hdf4
+
+SOURCE_NAME        = hdf-4
+SOURCE_VERSION     = $(VERSION)
+SOURCE_SUFFIX      = tar.gz
+SOURCE_PKG         = $(SOURCE_NAME).$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+ZLIB_NAME          = zlib
+ZLIB_VERSION       = 1.2.7
+ZLIB_SUFFIX        = tar.gz
+ZLIB_PKG           = $(ZLIB_NAME).$(ZLIB_VERSION).$(SOURCE_SUFFIX)
+ZLIB_DIR           = $(ZLIB_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TAR_GZ_PKGS        = $(SOURCE_PKG) $(ZLIB_PKG)
