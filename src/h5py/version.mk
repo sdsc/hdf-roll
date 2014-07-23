a5c61ba0acd85ccb9433ea3c83651f4103a0c073
Jim Hayes <jhayes@sdsc.edu>
2014-07-23 08:32:58 -0700

    Build h5py only once, using the first entry in ROLLCOMPILER/MPI/NETWORK/PY.

--- src/h5py/version.mk
+++ src/h5py/version.mk
@@ -1,25 +1,29 @@
 ifndef ROLLCOMPILER
   ROLLCOMPILER = gnu
 endif
-COMPILERNAME := $(firstword $(subst /, ,$(ROLLCOMPILER)))
+FIRSTCOMPILER = $(firstword $(ROLLCOMPILER))
+COMPILERNAME := $(firstword $(subst /, ,$(FIRSTCOMPILER)))
 
 ifndef ROLLMPI
   ROLLMPI = eth
 endif
+FIRSTMPI = $(firstword $(ROLLMPI))
 
 ifndef ROLLNETWORK
   ROLLNETWORK = eth
 endif
+FIRSTNETWORK = $(firstword $(ROLLNETWORK))
 
 ifndef ROLLPY
   ROLLPY = python
 endif
+FIRSTPY = $(firstword $(ROLLPY))
 
 ifndef PYVERSION
   PYVERSION = 2.6
 endif
 
-NAME               = h5py_$(COMPILERNAME)_$(ROLLMPI)_$(ROLLNETWORK)_py$(PYVERSION)
+NAME               = h5py_$(COMPILERNAME)_$(FIRSTMPI)_$(FIRSTNETWORK)_py$(PYVERSION)
 VERSION            = 2.2.1
 RELEASE            = 3
 RPM.EXTRAS         = AutoReq:No
