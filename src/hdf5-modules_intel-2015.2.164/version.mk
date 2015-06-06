0535f4954d16081bb44d439ddd84822acad38857
Jerry Greenberg <jpg@comet-devel.sdsc.edu>
2015-06-06 09:40:36 -0700

    fixed package and module names

---
+++ src/hdf5-modules_intel-2015.2.164/version.mk
@@ -0,0 +1,19 @@
+ROLLCOMPILER = intel/2015.2.164
+COMPILERNAME := $(firstword $(subst /, ,$(ROLLCOMPILER)))
+
+ifndef PYVERSION
+  PYVERSION = 2.6
+endif
+
+PACKAGE     = hdf5
+CATEGORY    = applications
+
+NAME        = sdsc-$(PACKAGE)-modules_$(COMPILERNAME)
+RELEASE     = 0
+PKGROOT     = /opt/modulefiles/$(CATEGORY)/.$(COMPILERNAME)/$(PACKAGE)
+
+VERSION_SRC = $(REDHAT.ROOT)/src/$(PACKAGE)-intel-2015.2.164/version.mk
+VERSION_INC = version.inc
+include $(VERSION_INC)
+
+RPM.EXTRAS  = AutoReq:No
