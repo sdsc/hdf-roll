fb8a0a09a8057d318a2daea1d9fd098e208b518e
Jerry Greenberg <jpg@comet-devel.sdsc.edu>
2015-06-06 23:27:38 -0700

    fixed paths in modules files

--- src/hdf5-modules_intel-2015.2.164/version.mk
+++ src/hdf5-modules_intel-2015.2.164/version.mk
@@ -1,6 +1,6 @@
-ROLLCOMPILER = intel/2015.2.164
-COMPILERNAME := $(firstword $(subst /, ,$(ROLLCOMPILER)))
-ROLLMPI = openmpi_ib
+RCOMPILER = intel/2015.2.164
+CNAME := $(firstword $(subst /, ,$(RCOMPILER)))
+RMPI = openmpi_ib
 
 ifndef PYVERSION
   PYVERSION = 2.6
@@ -9,9 +9,9 @@ endif
 PACKAGE     = hdf5
 CATEGORY    = applications
 
-NAME        = sdsc-$(PACKAGE)-modules_$(COMPILERNAME)-2015.2.164
+NAME        = sdsc-$(PACKAGE)-modules_$(CNAME)-2015.2.164
 RELEASE     = 0
-PKGROOT     = /opt/modulefiles/$(CATEGORY)/.$(COMPILERNAME)/$(PACKAGE)
+PKGROOT     = /opt/modulefiles/$(CATEGORY)/.$(CNAME)/$(PACKAGE)
 
 VERSION_SRC = $(REDHAT.ROOT)/src/$(PACKAGE)-intel-2015.2.164/version.mk
 VERSION_INC = version.inc
