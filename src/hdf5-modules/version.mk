ee1488ee0df202e8082b6e14449672b651eac7a4
Jerry Greenberg <jpg@sdsc.edu>
2018-09-18 14:42:00 -0700

    APS-842 added CXX environment variableto src/hdf5-1.8/Makefile and added an extra version to the hdf5 module

--- src/hdf5-modules/version.mk
+++ src/hdf5-modules/version.mk
@@ -2,12 +2,14 @@ PACKAGE     = hdf5
 CATEGORY    = applications
 
 NAME        = sdsc-$(PACKAGE)-modules
-RELEASE     = 10
+RELEASE     = 11
 PKGROOT     = /opt/modulefiles/$(CATEGORY)/$(PACKAGE)
 
 VERSION_SRC = $(REDHAT.ROOT)/src/$(PACKAGE)/version.mk
 VERSION_INC = version.inc
 include $(VERSION_INC)
+
+EXTRA_MODULE_VERSIONS = 1.8.21
 
 RPM.EXTRAS  = AutoReq:No\nObsoletes:sdsc-hdf5-modules_gnu,sdsc-hdf5-modules_intel,sdsc-hdf5-modules_pgi
 RPM.PREFIX  = $(PKGROOT)
