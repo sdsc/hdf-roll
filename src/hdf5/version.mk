145b1e9701ddec34916592fbb42538fb12e887f6
Jerry Greenberg <jpg@sdsc.edu>
2018-09-19 12:38:56 -0700

    APS-842 fix more problems with install paths and add new license file

--- src/hdf5/version.mk
+++ src/hdf5/version.mk
@@ -11,7 +11,7 @@ MPINAME := $(firstword $(subst /, ,$(ROLLMPI)))
 NAME           = sdsc-hdf5_$(COMPILERNAME)_$(MPINAME)
 VERSION        = 1.10.3
 RELEASE        = 0
-PKGROOT        = /opt/hdf5/$(COMPILERNAME)/$(MPINAME)
+PKGROOT        = /opt/hdf5/$(VERSION)/$(COMPILERNAME)/$(MPINAME)
 
 SRC_SUBDIR     = hdf5
 
@@ -21,7 +21,7 @@ SOURCE_SUFFIX  = tar.bz2
 SOURCE_PKG     = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
 SOURCE_DIR     = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
 
-TAR_BZ2        = $(SOURCE_PKG)
+TAR_BZ2_PKGS   = $(SOURCE_PKG)
 
 RPM.EXTRAS     = AutoReq:No
 RPM.PREFIX     = $(PKGROOT)
