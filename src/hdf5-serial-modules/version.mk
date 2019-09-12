6530ed0256b308e85c3c5f805124279183e14881
Jerry Greenberg <jpg@sdsc.edu>
2019-09-12 14:42:30 -0700

    APS-1040 fix syntax error in hdf5-serial-modules version.mk

--- src/hdf5-serial-modules/version.mk
+++ src/hdf5-serial-modules/version.mk
@@ -3,7 +3,7 @@ CATEGORY    = applications
 
 NAME        = sdsc-$(PACKAGE)-modules
 RELEASE     = 0
-PKGROOT     = /opt/modulefiles/$(CATEGORY)/$(PACKAGE
+PKGROOT     = /opt/modulefiles/$(CATEGORY)/$(PACKAGE)
 
 VERSION_SRC = $(REDHAT.ROOT)/src/$(PACKAGE)/version.mk
 VERSION_INC = version.inc
